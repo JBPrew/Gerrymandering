b7f61be6cdc2e079d9f3b0e4430e4b7afe3a5945
John Prewitt <john.prewitt1233@gcpsk12student.org>
2022-11-28 17:56:13 -0500

    Skeleton Code

---
+++ main.py
@@ -0,0 +1,20 @@
+#import libraries
+import numpy as np 
+import matplotlib.pyplot as plt 
+import pandas as pd 
+
+#load data
+df = pd.read_csv("NCPopulation.csv")
+df.columns()
+
+
+#data preparation
+
+
+#intialize model
+
+
+#score of map
+
+
+#create  map
